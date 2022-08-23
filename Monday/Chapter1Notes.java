8da4353af96aec8e8017a0d80725530fa0af5b44
Dylan Kosloski <dtkosloski@stu.naperville203.org>
2022-08-23 15:07:49 -0500

    Learned BluJ and took Notes

---
+++ Monday/Chapter1Notes.java
@@ -0,0 +1,37 @@
+
+/**
+ * Dylan Kosloski
+ * 8/23/22
+ * Chapter 1 Notes
+ * Class notes on basic concepts
+ * Who helped me: Mr. Hayes
+ */
+public class Chapter1Notes
+{
+    /**
+     * this is out code notes
+     */
+    public static void main(String[] args)
+    {
+        //basic data types
+        double number = 8.9,number2;
+        int x = 9,y,z;
+        boolean choice = false;
+        char letter = 'b';
+        
+        float number3 = 8;
+        long n2 = 3;
+        short n3 = 7;
+        
+        //these are Classes and Objects
+        //Class ID        Memory    Constructor
+        //Name  
+        Integer n1 =      new       Integer(47);
+        String  word =    new       String("Dylan");
+        Double  number4 = new       Double(6.7);
+        
+        System.out.print(word.length());
+        
+    
+    }
+}
